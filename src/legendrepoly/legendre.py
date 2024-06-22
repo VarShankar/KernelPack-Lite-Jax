ffb3aafaba8b8b34fd440f25c732f82022268d2d
Varun-Ubuntu <varun.shankar2001@gmail.com>
2024-06-21 22:08:50 -0600

    Added Legendre total degree approximation

---
+++ src/legendrepoly/legendre.py
@@ -0,0 +1,56 @@
+import jax
+import jax.numpy as jnp
+from jax import grad, vmap, jit
+from itertools import combinations_with_replacement, permutations
+
+
+@jit
+def legendre_poly(n, x):
+    P_n_minus_1 = jnp.ones_like(x)
+    P_n = x
+    def body(k, val):
+        P_n_minus_1, P_n = val
+        P_n_plus_1 = ((2*k - 1) * x * P_n - (k - 1) * P_n_minus_1) / k
+        return P_n, P_n_plus_1
+    P_n = jax.lax.fori_loop(2, n+1, body, (P_n_minus_1, P_n))[1]
+    return jnp.where(n == 0, P_n_minus_1, jnp.where(n == 1, x, P_n))
+
+def generate_multi_indices(l, d):
+    indices = set()
+    for comb in combinations_with_replacement(range(l + 1), d):
+        if sum(comb) <= l:
+            permutations_set = set(permutations(comb))
+            indices.update(permutations_set)
+    
+    # Sort indices by total degree and then lexicographically within each total degree
+    indices = sorted(indices, key=lambda x: (sum(x), x[::-1]))
+    return jnp.array(indices)
+
+def poly_eval(degrees, some_poly, x):
+    legendre_values = [some_poly(degree, x[i]) for i, degree in enumerate(degrees)]
+    return jnp.prod(jnp.array(legendre_values), axis=0)
+
+def total_degree_legendre_poly(xs, multi_indices):
+    def vandermonde_single_point(x):
+        return vmap(lambda degrees: poly_eval(degrees, legendre_poly, x))(multi_indices)
+    
+    vandermonde = vmap(vandermonde_single_point)(xs)
+    return vandermonde
+
+    
+# def gauss_legendre_zeros_1d(n):
+#     #Calculate the zeros of the nth degree Legendre polynomial (Gauss-Legendre nodes)."""
+#     zeros, _ = roots_legendre(n)
+#     return zeros
+
+def tensor_product_grid(ndims, n, univariate_grid_func):
+    #Generate tensor-product Legendre zeros for a given number of dimensions and polynomial order."""
+    # Get 1D zeros
+    zeros_1d = univariate_grid_func(n)[0]
+    
+    # Create a meshgrid of the zeros for the given number of dimensions
+    grids = jnp.meshgrid(*[zeros_1d]*ndims, indexing='ij')
+    
+    # Reshape the grids to create the tensor-product points
+    tensor_product_zeros = jnp.stack(grids, axis=-1).reshape(-1, ndims)
+    return tensor_product_zeros    
---
+++ src/legendrepoly/test_legendre_poly.py
@@ -0,0 +1,57 @@
+import math
+import jax.numpy as jnp
+import legendre as lg
+from scipy.special import roots_legendre
+
+# Example usage
+def vandermonde_test(l=2,d=2):
+    print("\nxxxxxxxxx Testing Legendre-Vandermonde matrix xxxxxxxxxxx")
+    multi_indices = lg.generate_multi_indices(l, d)
+    xs = jnp.array([[0.0, 0.0], [0.5, -0.5]])  
+    vandermonde_matrix = lg.total_degree_legendre_poly(xs, multi_indices)    
+    print(vandermonde_matrix)    
+    print("xxxxxxxxxx Done!\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+def interpolation_test(xs, xe, l=2,d=1,N=2,f = lambda x: 1./(1+ 25*x**2)):
+    print("\nxxxxxxxxx Testing interpolation xxxxxxxxxxxxxxxxxxxxxxxxxx")
+    #Get multi indices
+    multi_indices = lg.generate_multi_indices(l,d)
+
+    #Build the Vandermonde matrix
+    V = lg.total_degree_legendre_poly(xs,multi_indices)
+
+    #Evaluate rhs function on that grid
+    y = f(xs)
+
+    #Solve a linear system to get polynomial coefficients
+    c = jnp.linalg.solve(V,y)
+
+    #Get the Vandermonde evaluation matrix
+    Ve = lg.total_degree_legendre_poly(xe,multi_indices)
+
+    #Evaluate the interpolant
+    ye_approx = jnp.matmul(Ve,c)
+
+    #Evaluate true rhs
+    ye = f(xe)
+
+    # print an error
+    print("Relative l2 error", jnp.linalg.norm(ye-ye_approx)/jnp.linalg.norm(ye))
+    print("\nxxxxxxxxxx Done!\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    
+
+# Total degree approximation test
+degree = 75;
+dim = 1;
+N = math.comb(degree+dim,dim)
+Ne = 1000
+
+#Get a grid
+xs,_ = roots_legendre(N)
+xs = xs.reshape(N,dim)
+
+# Get an evaluation grid        
+xe = jnp.array(jnp.linspace(-1,1,Ne)).reshape(Ne,dim)
+
+f = lambda x: 1./(1+ 25*x**2)
+interpolation_test(xs,xe, degree,dim,N, f)
